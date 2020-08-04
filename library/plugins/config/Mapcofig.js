0ce5bc4ea513c4202555fe560f69fce72e3d7444
thiswildidea <gaqo@outlook.com>
2020-08-04 12:37:48 +0800

    添加了falsh3d

--- library/plugins/config/Mapcofig.js
+++ library/plugins/config/Mapcofig.js
@@ -2,7 +2,7 @@ var Mapcofig = /** @class */ (function () {
     // tslint:disable-next-line:no-empty
     function Mapcofig() {
     }
-    Mapcofig.jsapi = 'http://10.108.3.16/smiapi/arcgis';
+    Mapcofig.jsapi = 'http://10.201.37.225:8080/smiapi/arcgis';
     Mapcofig.jsapiversion4X = '4.15';
     return Mapcofig;
 }());
